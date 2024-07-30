264fe32880a2af3e854a54121369aad10492fba2
Sergio Bernal <sergiobernal909@gmail.com>
2024-07-30 11:39:16 -0600

    refactor: update colors to matchnigh-owl palette

--- src/git-status.sh
+++ src/git-status.sh
@@ -11,8 +11,8 @@ source "$CURRENT_DIR/themes.sh"
 
 cd "$1" || exit 1
 RESET="#[fg=${THEME[foreground]},bg=${THEME[background]},nobold,noitalics,nounderscore,nodim]"
-BRANCH=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
-STATUS=$(git status --porcelain 2>/dev/null | grep -cE "^(M| M)")
+BRANCH=$(git rev-parse --abbrev-ref HEAD 2> /dev/null)
+STATUS=$(git status --porcelain 2> /dev/null | grep -cE "^(M| M)")
 
 SYNC_MODE=0
 NEED_PUSH=0
@@ -27,7 +27,7 @@ STATUS_DELETIONS=""
 STATUS_UNTRACKED=""
 
 if [[ $STATUS -ne 0 ]]; then
-  DIFF_COUNTS=($(git diff --numstat 2>/dev/null | awk 'NF==3 {changed+=1; ins+=$1; del+=$2} END {printf("%d %d %d", changed, ins, del)}'))
+  DIFF_COUNTS=($(git diff --numstat 2> /dev/null | awk 'NF==3 {changed+=1; ins+=$1; del+=$2} END {printf("%d %d %d", changed, ins, del)}'))
   CHANGED_COUNT=${DIFF_COUNTS[0]}
   INSERTIONS_COUNT=${DIFF_COUNTS[1]}
   DELETIONS_COUNT=${DIFF_COUNTS[2]}
@@ -68,7 +68,7 @@ if [[ $SYNC_MODE -eq 0 ]]; then
     fi
 
     # Check if the remote branch is ahead of the local branch
-    REMOTE_DIFF="$(git diff --numstat "${BRANCH}" "origin/${BRANCH}" 2>/dev/null)"
+    REMOTE_DIFF="$(git diff --numstat "${BRANCH}" "origin/${BRANCH}" 2> /dev/null)"
     if [[ -n $REMOTE_DIFF ]]; then
       SYNC_MODE=3
     fi
@@ -77,20 +77,21 @@ fi
 
 # Set the status indicator based on the sync mode
 case "$SYNC_MODE" in
-1)
-  REMOTE_STATUS="$RESET#[bg=${THEME[background]},fg=${THEME[bred]},bold]▒ 󱓎"
-  ;;
-2)
-  REMOTE_STATUS="$RESET#[bg=${THEME[background]},fg=${THEME[red]},bold]▒ 󰛃"
-  ;;
-3)
-  REMOTE_STATUS="$RESET#[bg=${THEME[background]},fg=${THEME[magenta]},bold]▒ 󰛀"
-  ;;
-*)
-  REMOTE_STATUS="$RESET#[bg=${THEME[background]},fg=${THEME[green]},bold]▒ "
-  ;;
+  1)
+    REMOTE_STATUS="$RESET#[bg=${THEME[background]},fg=${THEME[bred]},bold]▒ 󱓎"
+    ;;
+  2)
+    REMOTE_STATUS="$RESET#[bg=${THEME[background]},fg=${THEME[red]},bold]▒ 󰛃"
+    ;;
+  3)
+    REMOTE_STATUS="$RESET#[bg=${THEME[background]},fg=${THEME[magenta]},bold]▒ 󰛀"
+    ;;
+  *)
+    REMOTE_STATUS="$RESET#[bg=${THEME[background]},fg=${THEME[green]},bold]▒ "
+    ;;
 esac
 
 if [[ -n $BRANCH ]]; then
-  echo "$REMOTE_STATUS $RESET$BRANCH $STATUS_CHANGED$STATUS_INSERTIONS$STATUS_DELETIONS$STATUS_UNTRACKED"
+  # echo "$REMOTE_STATUS $RESET$BRANCH $STATUS_CHANGED$STATUS_INSERTIONS$STATUS_DELETIONS$STATUS_UNTRACKED"
+  echo "$REMOTE_STATUS $RESET$BRANCH"
 fi
